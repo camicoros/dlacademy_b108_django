3279aff58d637d4e3f86b81908abf4bcea44841d
d.saushkin <d.saushkin@directline.digital>
2022-11-27 21:17:53 +0400

    Add posts app and post model

---
+++ instagrammik/posts/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+from core.models import CustomUser
+
+
+'''
+! не забываем добавить posts в INSTALLED_APPS в settings.py
+'''
+
+
+def post_image_path(instance, filename):
+    user_id = instance.id
+    return "posts/user-{}/{}".format(user_id, filename)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='posts')
+    description = models.TextField(max_length=1000, blank=True)
+    image = models.ImageField(upload_to=post_image_path)
+    date_pub = models.DateTimeField(auto_now_add=True)
+    date_edit = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(CustomUser, related_name='users_like_it', blank=True)
